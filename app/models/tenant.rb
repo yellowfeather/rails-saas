30b0f7899a0c85c655e96c944c00c76288048a90
Chris Richards <chris.richards@yellowfeather.co.uk>
2012-12-11 13:53:12 +0000

    Adds validation and index to tenant

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,5 +1,10 @@
 class Tenant < ActiveRecord::Base
   attr_accessible :name, :subdomain
+  validates_presence_of :subdomain
+  validates_format_of :subdomain, with: /^[a-z0-9_]+$/, message: 'must be lowercase alphanumerics only'
+  validates_length_of :subdomain, maximum: 32, message: 'exceeds maximum of 32 characters'
+  validates_exclusion_of :subdomain, in: ['www', 'mail', 'ftp'], message: 'is not available'
+  # Note: uniqueness will be caught at the db level
 
   def self.current_id=(id)
     Thread.current[:tenant_id] = id
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206175942) do
+ActiveRecord::Schema.define(:version => 20121210151507) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(:version => 20121206175942) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "tenants", ["subdomain"], :name => "index_tenants_on_subdomain", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email",                                :default => "", :null => false
