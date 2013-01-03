9f8a3d39040a347958dd5eeff2f1281e3428a011
Chris Richards <chris.richards@yellowfeather.co.uk>
2013-01-03 12:32:38 +0000

    Adds quantity to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   resourcify
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :quantity
 
   default_scope { where(tenant_id: Tenant.current_id) if Tenant.current_id }
 end
