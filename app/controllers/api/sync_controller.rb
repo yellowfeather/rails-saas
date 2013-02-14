8e18a9fc8b3916b0e762a37bd8614a32041aa6bb
Chris Richards <chris.richards@yellowfeather.co.uk>
2013-02-14 13:17:36 +0000

    Returns all if last_synced parameter not present

--- app/controllers/api/sync_controller.rb
+++ app/controllers/api/sync_controller.rb
@@ -11,28 +11,45 @@ module Api
     end
 
     def created
-      paginated_collection Product
-        .where('created_at >= ?', params[:last_synced])
+      products = Product
         .order('created_at ASC')
         .page(params[:page])
         .per(DEFAULT_PAGE_SIZE)
+
+      if params.has_key?(:last_synced)
+        products = products.where('created_at >= ?', params[:last_synced])
+      end
+
+      paginated_collection products
     end
 
     def updated
-      paginated_collection Product
-        .where('created_at < ?', params[:last_synced])
-        .where('updated_at >= ?', params[:last_synced])
+      products = Product
         .order('updated_at ASC')
         .page(params[:page])
         .per(DEFAULT_PAGE_SIZE)
+
+      if params.has_key?(:last_synced)
+        products = products
+          .where('created_at < ?', params[:last_synced])
+          .where('updated_at >= ?', params[:last_synced])
+      end
+
+      paginated_collection products
     end
 
     def deleted
-      paginated_collection Tombstone
-        .where('created_at >= ?', params[:last_synced])
+      tombstones = Tombstone
         .order('created_at ASC')
         .page(params[:page])
         .per(DEFAULT_PAGE_SIZE)
+
+      if params.has_key?(:last_synced)
+        tombstones = tombstones.where('created_at >= ?', params[:last_synced])
+      end
+
+      paginated_collection tombstones
     end
   end
 end
+
